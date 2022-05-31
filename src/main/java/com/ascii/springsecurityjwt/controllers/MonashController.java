bdcc7dd5cb5e9b2d9ae9e14f9f08cc15d1a7d9cc
Allen <asciicode@gmail.com>
2022-06-01 00:21:27 +1200

    first commit

--- src/main/java/com/ascii/springsecurityjwt/controllers/MonashController.java
+++ src/main/java/com/ascii/springsecurityjwt/controllers/MonashController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import com.ascii.springsecurityjwt.services.FirestoreService;
 import com.ascii.springsecurityjwt.util.JwtUtil;
 
 @RestController
+@CrossOrigin(origins = { "${settings.cors_origi}" })
 public class MonashController
 {
 
