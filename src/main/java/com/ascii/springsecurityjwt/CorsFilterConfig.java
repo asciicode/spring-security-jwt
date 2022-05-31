c8633be4b177a59c544b3e65d9ec6158cca925de
Allen <asciicode@gmail.com>
2022-06-01 09:21:24 +1200

    first commit

---
+++ src/main/java/com/ascii/springsecurityjwt/CorsFilterConfig.java
@@ -0,0 +1,35 @@
+package com.ascii.springsecurityjwt;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsFilterConfig
+{
+
+  @Value("${settings.cors_origin}")
+  private String allowedOrigin;
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> simpleCorsFilter()
+  {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList(allowedOrigin));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
+  }
+}
